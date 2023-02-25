0247297bb4e864b479f9302ecc686c8b3ef627b8
Alwan Candra Fadila <113606659+alwancandra1@users.noreply.github.com>
2023-02-25 17:43:38 +0700

    Update app.py

--- app.py
+++ app.py
@@ -64,7 +64,7 @@ if st.button('Predict Price'):
 X_res = int(resolution.split('x')[0])
 Y_res = int(resolution.split('x')[1])
 #ppi = ((X_res**2)+(Y_res**2))**0.5/screen_size
-ppi = ((X_res**2)+(Y_res**2))**0.5
+ppi = ((X_res**2)+(Y_res**2))**0.5/screen_size
 #query = np.array([company,type,ram,weight,touchscreen,ips,ppi,cpu,hdd,ssd,gpu,os])
 query = np.array([company,screen_size,ram,weight,touchscreen,ips,ppi,cpu,hdd,ssd,gpu,os])
 
