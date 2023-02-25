5a448cffca41219b54a247d361b122074c389d21
Alwan Candra Fadila <113606659+alwancandra1@users.noreply.github.com>
2023-02-25 16:49:40 +0700

    Update app.py

--- app.py
+++ app.py
@@ -65,7 +65,7 @@ if st.button('Predict Price'):
 X_res = int(resolution.split('x')[0])
 Y_res = int(resolution.split('x')[1])
 #ppi = ((X_res**2)+(Y_res**2))**0.5
-ppi=1
+ppi=ram
 query = np.array([company,type,ram,weight,touchscreen,ips,ppi,cpu,hdd,ssd,gpu,os])
 
 # Prediksi Harga Laptop
